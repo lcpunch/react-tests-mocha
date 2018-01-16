c37d064e758a1233c30d5af6434e3b76715f0491
lcpunch <lcpunchirolli@gmail.com>
2018-01-16 16:35:39 -0500

    Initial commit

---
+++ src/components/comment_box.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class CommmentBox extends Component {
+
+    render() {
+
+        return(
+            <div className="comment-box">
+                <textarea />
+                <button> Submit Comment</button>
+            </div>
+        );
+    }
+}
