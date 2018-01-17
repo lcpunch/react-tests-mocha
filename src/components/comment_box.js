a4259d869f05e8ce929e6126879713e594363f82
lcpunch <lcpunchirolli@gmail.com>
2018-01-17 13:27:42 -0500

    second

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 
 export default class CommmentBox extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = { comment: ''};
+    }
+
+    handleChange(event) {
+        this.setState({ comment: event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.setState({ comment: '' });
+    }
+
     render() {
 
         return(
-            <div className="comment-box">
-                <textarea />
-                <button> Submit Comment</button>
-            </div>
+            <form onSubmit={this.handleSubmit.bind(this)} className="comment-box">
+                <textarea
+                    value={this.state.comment}
+                    onChange={this.handleChange.bind(this)} />
+                <button action="submit">Submit Comment</button>
+            </form>
         );
     }
 }
