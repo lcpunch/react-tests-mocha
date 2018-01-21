658514ba20d70b5420aaebc8e198a2693725435d
lcpunch <lcpunchirolli@gmail.com>
2018-01-21 15:16:39 -0500

    finished testing section with mocha and chai

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class CommmentBox extends Component {
+class CommentBox extends Component {
 
     constructor(props) {
         super(props);
@@ -15,6 +17,7 @@ export default class CommmentBox extends Component {
     handleSubmit(event) {
         event.preventDefault();
 
+        this.props.saveComment(this.state.comment);
         this.setState({ comment: '' });
     }
 
@@ -22,11 +25,17 @@ export default class CommmentBox extends Component {
 
         return(
             <form onSubmit={this.handleSubmit.bind(this)} className="comment-box">
+                <h4>Add a comment</h4>
                 <textarea
                     value={this.state.comment}
                     onChange={this.handleChange.bind(this)} />
-                <button action="submit">Submit Comment</button>
+                <div>
+                    <button action="submit">Submit Comment</button>
+                </div>
             </form>
         );
     }
 }
+
+
+export default connect(null, actions)(CommentBox);
